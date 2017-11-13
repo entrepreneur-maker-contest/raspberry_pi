f1b9917f9315521cf1fe17a9ff5b0ecbcd4a0a86
zaeval <h3065@naver.com>
2017-11-14 06:58:46 +0900

    second

--- raspberry.py
+++ raspberry.py
@@ -3,10 +3,12 @@ from PyQt5 import QtGui
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from time import sleep
 R=185
 G=57
 B=65
-
+drink_type = "None"
+kalori = "0"
 class MyLabel(QLabel):
     def __init__(self, text=None,width=0,height=0):
         super(self.__class__, self).__init__()
@@ -23,15 +25,18 @@ class MyLabel(QLabel):
         painter.end()
 
 
-class Main(QWidget):
+class First(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self.label1 = MyLabel("INGREDIENT",48,30)
         self.label1.setStyleSheet("font-size: 20px;font-weight: 300;text-align: center;color: rgb(255, 255, 255);")
 
-        self.label2 = MyLabel("BEER",57,30)
+        self.label2 = MyLabel(drink_type,57,27)
         self.label2.setStyleSheet("font-size: 40px;font-weight: 300;line-height: 0.62;text-align: center;color: rgb(255, 255, 255);")
+
+        self.label3 = MyLabel(kalori+"kcal",50,20)
+        self.label3.setStyleSheet("font-size: 10px;font-weight: 500;text-align: center;color: rgb(255, 255, 255);")
 
         pixmap = QPixmap('raspberry-icon.png')
         transform = QTransform().rotate(90)
@@ -43,10 +48,11 @@ class Main(QWidget):
         hBoxLayout = QHBoxLayout()
 
 
+        hBoxLayout.addWidget(self.label3)
+
         hBoxLayout.addWidget(self.label2)
 
         hBoxLayout.addWidget(self.label1)
-
 
         hBoxLayout.addWidget(self.icon)
 
@@ -58,7 +64,31 @@ class Main(QWidget):
         color = QColor()
         palette.setColor(role, color.fromRgb(B, G, R))
         self.setPalette(palette)
+class Second(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
+        self.label1 = MyLabel("INGREDIENT",48,30)
+        self.label1.setStyleSheet("font-size: 20px;font-weight: 300;text-align: center;color: rgb(255, 255, 255);")
+
+        self.label2 = MyLabel("BEER",57,30)
+        self.label2.setStyleSheet("font-size: 40px;font-weight: 300;line-height: 0.62;text-align: center;color: rgb(255, 255, 255);")
+
+        hBoxLayout = QHBoxLayout()
+
+
+        hBoxLayout.addWidget(self.label2)
+
+        hBoxLayout.addWidget(self.label1)
+
+        self.setLayout(hBoxLayout)
+        self.showFullScreen()
+
+        palette = self.palette()
+        role = self.backgroundRole()
+        color = QColor()
+        palette.setColor(role, color.fromRgb(B, G, R))
+        self.setPalette(palette)
 
 
 def buttonClicked(self):
@@ -69,6 +99,22 @@ if __name__ == '__main__':
     import sys
 
     app = QApplication(sys.argv)
-    main = Main()
-    main.show()
+    first = First()
+    second = Second()
+    i = 0
+    first_check = False
+    while True:
+        if first_check:
+            sleep(3)
+        else:
+            first_check=True
+        if i%2:
+            first.show()
+            second.hide()
+        else:
+            second.show()
+            first.hide()
+
+        i = (i+1)%2
+
     sys.exit(app.exec_())
